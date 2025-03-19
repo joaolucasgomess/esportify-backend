7de1139d05580019e2ae021280d1267f7e990890
joaolucasgomess <jlnapole12@gmail.com>
2025-03-19 17:56:00 -0300

    chore: remove export of the pool

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-export const pool = new Pool({
+const pool = new Pool({
     connectionString: process.env.DB_URL!,
 });
 
