4840882224fe185b4f79634c904125831f4f5640
joaolucasgomess <jlnapole12@gmail.com>
2025-03-30 22:40:06 -0300

    chore: update index from db for seeds and new packge-lock

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
+// import { generatedId } from "../utils/idGenerator";
 
 const pool = new Pool({
     connectionString: process.env.DB_URL!,
@@ -10,18 +11,18 @@ const pool = new Pool({
 export const db = drizzle({ client: pool, schema: schema });
 
 // async function main() {
-//     const cnpjValid1: typeof cnpjValid.$inferInsert = {
+//     const cnpjValid1: typeof schema.cnpjValid.$inferInsert = {
 //         id: generatedId(),
 //         cnpj: "11111111111111",
 //     };
 
-//     const cnpjValid2: typeof cnpjValid.$inferInsert = {
+//     const cnpjValid2: typeof schema.cnpjValid.$inferInsert = {
 //         id: generatedId(),
 //         cnpj: "22222222222222",
 //     };
 
-//     await db.insert(cnpjValid).values(cnpjValid1);
-//     await db.insert(cnpjValid).values(cnpjValid2);
+//     await db.insert(schema.cnpjValid).values(cnpjValid1);
+//     await db.insert(schema.cnpjValid).values(cnpjValid2);
 // }
 
 // main();
